932ad7504503a662514864004d9520e4b1c1a16e
Lucas Vera <lucas.s.vera@hotmail.com>
2022-05-19 18:52:09 -0300

    Integracion funcionalidad tickets

---
+++ codigo.js
@@ -0,0 +1,24 @@
+function calcularTotal(){
+    let categoria = document.getElementById("opcionDescuento").value;
+    let cantidad = document.getElementById("inputCantidad").value;
+    let descuento;
+    
+    if(categoria == 1){ 
+        descuento=0.8;
+    }else if(categoria == 2)
+    {   descuento=0.5;
+    }else{ 
+        descuento= 0.15;
+    }
+
+    let totalAPagar = 200*cantidad-(200*descuento*cantidad);
+
+    let cartel = document.getElementById("cartelTotal");
+    cartel.value="  Total a Pagar: $ "+totalAPagar;
+}
+
+var btnEnviar = document.getElementById("botonResumen");
+
+btnEnviar.addEventListener("click",()=>{
+    this.preventDefault();
+})
